1d628525276496c7194dc61baa06a5d2cd7e33f2
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-08-19 10:46:29 +0800

    init

---
+++ apps/frontstage-e2e/cypress.config.ts
@@ -0,0 +1,15 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: { default: 'pnpm exec nx serve frontstage' },
+      ciWebServerCommand: 'nx run frontstage:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
